e946eb934148627b8dbf4036fd76e55c273d6a07
James Wrest <jrwrest@gmail.com>
2022-06-24 19:02:07 +0200

    -

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
                       uniqueness: true
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
- # Returns the hash digest of the given string.
+ # Returns the hash digest of the given string.-
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
